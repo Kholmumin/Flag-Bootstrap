60c4b82eb8384261e89e40ae53283b05ce48e16f
Kholmumin <80518819+Kholmumin@users.noreply.github.com>
2022-08-22 14:54:30 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+elDark = document.querySelector(".dark");
+// elBody = document.querySelector("#site-body");
+
+
+elDark.addEventListener("click",function(){
+    document.body.classList.toggle("theme-dark")
+})
